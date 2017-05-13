4c8c36694500296da509ea360b9931e0a98f4360
Denis Alaev <disaalaev@gmail.com>
2017-05-13 14:08:23 +0800

    5.1

---
+++ semester2/hw5/task1/src/spbu/sem2/hw5/task1/Main.java
@@ -0,0 +1,23 @@
+package spbu.sem2.hw5.task1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("calculator.fxml"));
+        primaryStage.setTitle("Simple calculator");
+        primaryStage.setScene(new Scene(root, 1000, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
